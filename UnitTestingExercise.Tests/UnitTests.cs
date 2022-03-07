d01d72051550e14e1a89774ac96303c952c46467
Valdir Lima <valdirlima508@gmail.com>
2022-03-07 16:47:07 -0500

    initial commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,97 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1, 3)]
+        [InlineData(-1, -2, -3, -6)]
+        [InlineData(0, 0, 0, 0)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var addNum = new UnitTestMethods();
 
             //Act
+            int actual = addNum.Add(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 0)]
+        [InlineData(-1, 3, -4)]
+        [InlineData(-1, -3, 2)]
+        [InlineData(0, 0, 0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var subNum = new UnitTestMethods();
 
             //Act
+            int actual = subNum.Subtract(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1)]
+        [InlineData(-1, 5, -5)]
+        [InlineData(5, 0, 0)]
+        [InlineData(0, 0, 0)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var mulNum = new UnitTestMethods();
 
             //Act
+            int actual = mulNum.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 1, 5)]
+        [InlineData(-25, 5, -5)]
+        [InlineData(0, 5, 0)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var divSum = new UnitTestMethods();
 
             //Act
+            int actual = divSum.Divide(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void DoStuff()
         {
             //Arrange
+            var stuff = new UnitTestMethods();
 
             //Act
+            string actual = stuff.DoStuff();
 
             //Assert
-
+            Assert.Equal("Doing Stuff", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void Greetings()
         {
             //Arrange
+            var quote = new UnitTestMethods();
 
             //Act
+            var actual = quote.Greetings();
 
             //Assert
+            Assert.Equal("Hello there friend!", actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,14 +4,14 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
 
@@ -21,6 +21,43 @@ namespace UnitTestingExercise
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
         
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
+
+        
+        public int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);   
+        }
+
+        
+        public int Divide(int num1, int num2)
+        {
+            return (num1 / num2); 
+        }
+
+        
+        public string DoStuff()
+        {
+            Console.WriteLine("Doing Stuff"); //Do i need both??
+            return "Doing Stuff";
+        }
+
+        
+        public string Greetings()
+        {
+            Console.WriteLine("Hello there friend!");
+            return "Hello there friend!";
+        }
+
+
     }
 }
